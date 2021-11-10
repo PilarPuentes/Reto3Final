ad089cffac4a94111439d3e37076579ea4a745bc
Pilar Puentes <maria.puentes.mt@correo.usa.edu.co>
2021-11-10 00:19:34 -0500

    Se actualiza codigo

--- src/main/java/Reto3/api/Reportes/StatusReservas.java
+++ src/main/java/Reto3/api/Reportes/StatusReservas.java
@@ -1,20 +1,20 @@
 package Reto3.api.Reportes;
 
 public class StatusReservas {
-    private int complete;
+    private int completed;
     private int cancelled;
 
-    public StatusReservas(int complete, int cancelled) {
-        this.complete = complete;
+    public StatusReservas(int completed, int cancelled) {
+        this.completed = completed;
         this.cancelled = cancelled;
     }
 
-    public int getComplete() {
-        return complete;
+    public int getCompleted() {
+        return completed;
     }
 
-    public void setComplete(int complete) {
-        this.complete = complete;
+    public void setCompleted(int completed) {
+        this.completed = completed;
     }
 
     public int getCancelled() {
